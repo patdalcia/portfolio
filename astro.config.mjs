1f32102e335d4774f1323be7593cd49544eb084f
patdalcia <patdalcia@gmail.com>
2025-05-24 16:32:13 -0700

    added images

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,11 +4,11 @@ import tailwindcss from "@tailwindcss/vite";
 
 import node from "@astrojs/node";
 
+import cloudflare from "@astrojs/cloudflare";
+
 // https://astro.build/config
 export default defineConfig({
   vite: {    plugins: [tailwindcss()],  },
 
-  adapter: node({
-    mode: "standalone",
-  }),
+  adapter: cloudflare(),
 });
