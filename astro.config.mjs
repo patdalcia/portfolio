779431cd9e8671143d1939677f685ef1a609f2ae
patdalcia <patdalcia@gmail.com>
2025-05-27 03:08:41 -0700

    added lines to config file to fix bug with react 19 and cloudflare deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -12,7 +12,16 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  vite: {    plugins: [tailwindcss()],  },
+  vite: {
+    plugins: [tailwindcss()],
+    resolve: {
+      // Use react-dom/server.edge instead of react-dom/server.browser for React 19.
+      // Without this, MessageChannel from node:worker_threads needs to be polyfilled.
+      alias: import.meta.env.PROD ? {
+        "react-dom/server": "react-dom/server.edge",
+      } : undefined,
+    },
+  },
   adapter: cloudflare(),
-  integrations: [react({include: [],})],
+  integrations: [react({ include: [], })],
 });
