9f9bc123b87ef472fd465dd9a619cb9494bc53e6
patdalcia <patdalcia@gmail.com>
2025-05-30 03:23:06 -0700

    finally got it working. Added Bunny CDN integration

--- src/components/react/Hero.jsx
+++ src/components/react/Hero.jsx
@@ -1,34 +1,18 @@
-import React from "react";
-
-import { getImage } from "astro:assets";
-import FrontShot from "../../assets/hero/FrontShot-min.png";
-import TopShot from "../../assets/hero/TopShot-min.png";
-import CloseShot from "../../assets/hero/CloseShot-min.png";
-import PotStill from "../../assets/hero/PotStill-min.png";
-const optimizedFrontShot = await getImage({ src: FrontShot });
-const optimizedTopShot = await getImage({ src: TopShot });
-const optimizedCloseShot = await getImage({ src: CloseShot });
-const optimizedPotStill = await getImage({ src: PotStill });
-
-const Images = [
-    optimizedFrontShot,
-    optimizedTopShot,
-    optimizedCloseShot,
-    optimizedPotStill,
-];
-const selectedImage = optimizedPotStill;
-
-// const selectedImage = Images[Math.floor(Math.random() * Images.length)];
 
 export default function Hero(props) {
+ 
+  const selectedImage = 'https://patdalcia-portfolio.b-cdn.net/hero/SelectedHero/PotStill-min.png'
+
     return (
-        <div
-            className="min-h-screen w-full bg-no-repeat bg-cover bg-center bg-blend-multiply flex-col"
+        <>
+            <div 
+            className="min-h-screen bg-center bg-cover"
             style={{
-                backgroundImage: `url(${selectedImage.src})`,
+                backgroundImage: `url(${selectedImage})`,
                 backgroundColor: `#808080`,
             }}>
-            {props.HeroContent}
-        </div>
+                {props.test}
+            </div>
+        </>
     )
 }
